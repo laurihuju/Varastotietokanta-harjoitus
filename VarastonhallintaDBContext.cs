264fa8e1dd5dcf0c6fbbe4c05161cde59a9701ce
Huju Lauri <s2000949@edu.bc.fi>
2022-10-07 17:14:35 +0300

    Made the db context connection string creation

---
+++ VarastonhallintaDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Varastotietokanta_harjoitus
+{
+    class VarastonhallintaDBContext : DbContext
+    {
+        private string address;
+        private string user;
+        private string password;
+
+        public VarastonhallintaDBContext(string address, string user, string password)
+        {
+            this.address = address;
+            this.user = user;
+            this.password = password;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer($"Server={address};Database=Varastonhallinta;User Id={user};Password={password};");
+        }
+    }
+}
