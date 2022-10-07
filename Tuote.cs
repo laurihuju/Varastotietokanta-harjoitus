7606104da7dc8b02f7d27febf012181028fc233b
Huju Lauri <s2000949@edu.bc.fi>
2022-10-07 17:25:00 +0300

    Created the Tuote table model

---
+++ Tuote.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Varastotietokanta_harjoitus
+{
+    class Tuote
+    {
+        [Key]
+        public int? ID { get; set; }
+        public string? Tuotenimi { get; set; }
+        public int Hinta { get; set; }
+        public int VarastoSaldo { get; set; }
+    }
+}
